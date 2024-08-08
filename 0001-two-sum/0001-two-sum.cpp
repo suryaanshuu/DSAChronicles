1cef0a97d375c5374da890cb88ab4034a2e852d5
Suryanshu Choudhary <94002793+suryaanshuu@users.noreply.github.com>
2024-08-08 16:15:04 +0530

    Time: 3 ms (98.48%), Space: 14.4 MB (17.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int y;
+//         vector<int> ans;
+        
+//         for(int i = 0; i < n; i++){
+//             for(int j = i+1; j < n; j++){
+//                 if (nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+// Using Hash Maps
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        int y;
-        vector<int> ans;
+        unordered_map<int, int> map;
+        vector<int> res;
         
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        for(int i = 0; i < nums.size(); i++){
+           int diff = target - nums[i];
+
+            if(map.find(diff) == map.end()){
+                map[nums[i]] = i;
+            }
+            else{
+                res.push_back(i);
+                res.push_back(map[diff]);
+                break;
             }
         }
-        return {};
+        return res;
     }
 };
