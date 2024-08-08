d8b25ee33b084967c6ef3b095885bf92637cd97a
Suryanshu Choudhary <94002793+suryaanshuu@users.noreply.github.com>
2024-08-08 15:36:52 +0530

    Time: 11 ms (37.31%), Space: 10 MB (8.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,43 @@
+// This was a basic approach
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+
+//         if(s==t){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+\
+// This approach uses Hash Maps (freqency maps)
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+    bool isAnagram(string s, string t) {    
+        unordered_map<char, int> sm;
+        unordered_map<char, int> tm;
+        if(s.length() != t.length()) return false;
+        else{
+            for(int i = 0; i < s.length(); i++){
+                sm[s[i]]++;
+            }
 
-        if(s==t){
-            return true;
+            for(int i = 0; i < t.length(); i++){
+                tm[t[i]]++;
+            }
+
+            for(auto ele: sm){
+                char ch1 = ele.first;
+                int freq1 = ele.second;
+                if(tm.find(ch1) != tm.end()){
+                    int freq2 = tm[ch1];
+                    if(freq1 != freq2) return false;
+                }
+                else return false;
+            }
         }
-        return false;
+        return true;
     }
 };
