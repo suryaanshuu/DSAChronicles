e3572e36991053533814669c00e2def612d0f610
Suryanshu Choudhary <94002793+suryaanshuu@users.noreply.github.com>
2024-08-17 22:29:26 +0530

    Time: 23 ms (21.6%), Space: 28.1 MB (8.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,34 +1,51 @@
-// Moore's Voting Algo is used here
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int count = 0;
-        int candidate;
-
-        for(int i = 0; i < n; i++){
-            if(count == 0){
-                candidate = nums[i];
-            }
-
-            if(nums[i] == candidate){
-                count++;
-            }
-            else if(nums[i] != candidate){
-                count--;
-            }
+        sort(nums.begin(), nums.end());
+        
+        if(n%2 == 0){
+            return nums[(n/2)-1];
         }
-
-        count = 0;
-        for(int num : nums){
-            if(num == candidate){
-                count++;
-            }
+        else{
+            return nums[(n/2)];
         }
-
-        if(count > nums.size() / 2) {
-            return candidate;
-        }
-    return 0;
+        return -1;
     }
 };
+
+// // Moore's Voting Algo is used here
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         int n = nums.size();
+//         int count = 0;
+//         int candidate;
+
+//         for(int i = 0; i < n; i++){
+//             if(count == 0){
+//                 candidate = nums[i];
+//             }
+
+//             if(nums[i] == candidate){
+//                 count++;
+//             }
+//             else if(nums[i] != candidate){
+//                 count--;
+//             }
+//         }
+
+//         int confirmCount = 0;
+//         for(int num : nums){
+//             if(num == candidate){
+//                 confirmCount++;
+//             }
+//         }
+
+//         if(confirmCount > nums.size() / 2) {
+//             return candidate;
+//         }
+//     return 0;
+//     }
+// };
+
