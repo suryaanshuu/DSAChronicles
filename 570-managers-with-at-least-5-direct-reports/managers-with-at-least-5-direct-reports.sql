87f0b8406aaec3ead027bbd94d338175d22748c4
Suryanshu Choudhary <94002793+suryaanshuu@users.noreply.github.com>
2024-09-16 16:38:37 +0530

    Time: 449 ms (18.14%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,17 @@
--- Write your PostgreSQL query statement below
+-- -- Write your PostgreSQL query statement below
+-- select e1.name
+-- from Employee e1
+-- join 
+-- (select managerId, count(managerId) as no_of_reports
+-- from Employee 
+-- group by managerId
+-- having count(managerId) >= 5
+-- ) as e2
+-- on e1.id = e2.managerId
+
 select e1.name
 from Employee e1
-join 
-(select managerId, count(managerId) as no_of_reports
-from Employee 
-group by managerId
-having count(managerId) >= 5
-) as e2
+join Employee e2
 on e1.id = e2.managerId
+group by e2.managerId, e1.name
+having count(e2.managerId) >= 5
